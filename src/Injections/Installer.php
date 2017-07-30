73e3d74319963a35cfe6730af8876c48b725a5e1
twilroad <269044570@qq.com>
2017-07-31 01:22:28 +0800

    add post handle logic for module installer

--- src/Injections/Installer.php
+++ src/Injections/Installer.php
@@ -6,9 +6,12 @@
  * @copyright (c) 2017, notadd.com
  * @datetime 2017-03-10 14:12
  */
+
 namespace Notadd\Member\Injections;
 
 use Notadd\Foundation\Module\Abstracts\Installer as AbstractInstaller;
+use Notadd\Member\Models\MemberGroup;
+use Notadd\Member\Models\MemberGroupRelation;
 
 /**
  * Class Installer.
@@ -24,9 +27,28 @@ class Installer extends AbstractInstaller
     }
 
     /**
+     * Post handle.
+     */
+    public function post()
+    {
+        MemberGroup::query()->create([
+            'identification' => 'admin',
+            'name' => '管理员组',
+        ]);
+        MemberGroup::query()->create([
+            'identification' => 'user',
+            'name' => '用户组组',
+        ]);
+        MemberGroupRelation::query()->create([
+            'group_id' => 1,
+            'member_id' => 1,
+        ]);
+    }
+
+    /**
      * @return array
      */
-    public function require ()
+    public function require()
     {
         return [];
     }
