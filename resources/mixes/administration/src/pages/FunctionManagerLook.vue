cb484fe9c3cddfd49795fa9d87d0099911113f4a
tianjign <tfanxi@163.com>
2017-11-08 14:27:06 +0800

    add look function module content of function manager

--- resources/mixes/administration/src/pages/FunctionManagerLook.vue
+++ resources/mixes/administration/src/pages/FunctionManagerLook.vue
@@ -7,6 +7,7 @@
             });
         },
         data() {
+            const self = this;
             return {
                 columns: [
                     {
@@ -82,6 +83,13 @@
                         width: 280,
                     },
                 ],
+                createModal: {
+                    function_name: '',
+                    parent_fun: '',
+                },
+                deleteModal: {
+                    name: 'ibenchu',
+                },
                 list: [
                     {
                         id: 1323,
@@ -96,6 +104,8 @@
                         name: 'benchu3',
                     },
                 ],
+                modal: false,
+                modalCreate: false,
                 pagination: {
                     count: 3,
                     current: 1,
@@ -104,6 +114,15 @@
                 parent: {
                     fun_name: '',
                     tab_name: '',
+                },
+                rules: {
+                    function_name: [
+                        {
+                            message: '功能不能为空',
+                            required: true,
+                            trigger: 'blur',
+                        },
+                    ],
                 },
             };
         },
@@ -139,6 +158,67 @@
                           show-elevator></page>
                 </div>
             </card>
+            <modal
+                    v-model="modal"
+                    title="删除" class="setting-modal-delete">
+                <div>
+                    <i-form ref="deleteModal" :model="deleteModal" :label-width="120">
+                        <row>
+                            <i-col class="first-row-title delete-file-tip">
+                                <span>确定要删除"{{ deleteModal.name }}"功能吗？删除后拥有此功能的角色将失去此功能。</span>
+                            </i-col>
+                        </row>
+                        <row>
+                            <i-col class="btn-group">
+                                <i-button type="ghost" class="cancel-btn"
+                                          @click.native="submitCancel">取消</i-button>
+                                <i-button :loading="loading" type="primary" class="cancel-btn"
+                                          @click.native="submitDelete">
+                                    <span v-if="!loading">确认</span>
+                                    <span v-else>正在删除…</span>
+                                </i-button>
+                            </i-col>
+                        </row>
+                    </i-form>
+                </div>
+            </modal>
+            <modal
+                    v-model="modalCreate"
+                    title="新增功能" class="setting-modal-delete setting-modal-action">
+                <div>
+                    <i-form ref="createModal" :model="createModal" :rules="rules" :label-width="110">
+                        <row>
+                            <i-col span="14">
+                                <form-item label="功能名称" prop="function_name">
+                                    <i-input v-model="createModal.function_name"></i-input>
+                                </form-item>
+                            </i-col>
+                        </row>
+                        <row>
+                            <i-col span="14">
+                                <form-item label="父级功能" prop="parent_fun">
+                                    <i-select v-model="createModal.parent_fun">
+                                        <i-option v-for="item in functionList"
+                                                  :value="item.value">{{ item.label }}
+                                        </i-option>
+                                    </i-select>
+                                </form-item>
+                            </i-col>
+                        </row>
+                        <row>
+                            <i-col span="14">
+                                <form-item>
+                                    <i-button :loading="loading" @click.native="submitCreate"
+                                              class="btn-group" type="primary">
+                                        <span v-if="!loading">确认提交</span>
+                                        <span v-else>正在提交…</span>
+                                    </i-button>
+                                </form-item>
+                            </i-col>
+                        </row>
+                    </i-form>
+                </div>
+            </modal>
         </div>
     </div>
 </template>
