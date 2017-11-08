db5bccc54aed15244c64ae886dacdfd20e8b34aa
twilroad <269044570@qq.com>
2017-11-08 15:17:40 +0800

    add definition of type for member information

---
+++ src/GraphQL/Types/MemberInformationType.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        TwilRoad <heshudong@ibenchu.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime      2017-11-08 15:16
+ */
+namespace Notadd\Member\GraphQL\Types;
+
+use Notadd\Foundation\GraphQL\Abstracts\Type as AbstractType;
+
+/**
+ * Class MemberInformationType.
+ */
+class MemberInformationType extends AbstractType
+{
+    /**
+     * @return array
+     */
+    public function fields()
+    {
+        return [];
+    }
+
+    /**
+     * @return string
+     */
+    public function name()
+    {
+        // TODO: Implement name() method.
+    }
+}
