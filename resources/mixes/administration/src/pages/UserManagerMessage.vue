e568097ebbf3cec0cb72648bce0cb86667bcc21b
tianjign <tfanxi@163.com>
2017-11-09 16:37:11 +0800

    add rule validator content

--- resources/mixes/administration/src/pages/UserManagerMessage.vue
+++ resources/mixes/administration/src/pages/UserManagerMessage.vue
@@ -13,12 +13,22 @@
             }
         },
         data() {
+            const reg = /^[0-9]*$/;
+            const validatorSort = (rule, value, callback) => {
+                if (!reg.test(value)) {
+                    callback(new Error('权限值为数字'));
+                } else {
+                    callback();
+                }
+            };
             return {
                 action: `${window.api}/zn/admin/upload`,
                 form: {
-                    image: '',
+                    intro: '',
                     name: '',
-                    sex: '1',
+                    sort: null,
+                    status: true,
+                    type: 0,
                 },
                 loading: false,
                 parent: {
@@ -26,14 +36,73 @@
                     type: '',
                 },
                 rules: {
-                    email: [
+                    name: [
                         {
-                            message: '邮箱格式不正确',
+                            message: '名称不能为空',
+                            required: true,
                             trigger: 'blur',
-                            type: 'email',
+                        },
+                    ],
+                    type: [
+                        {
+                            message: '类型不能为空',
+                            required: true,
+                            trigger: 'blur',
+                        },
+                    ],
+                    sort: [
+                        {
+                            trigger: 'blur',
+                            validator: validatorSort,
                         },
                     ],
                 },
+                types: [
+                    {
+                        label: '请选择信息类型',
+                        value: 0,
+                    },
+                    {
+                        label: '单行文本框',
+                        value: 1,
+                    },
+                    {
+                        label: '多行文本框',
+                        value: 2,
+                    },
+                    {
+                        label: '单选框',
+                        value: 3,
+                    },
+                    {
+                        label: '多选框',
+                        value: 4,
+                    },
+                    {
+                        label: '复选框',
+                        value: 5,
+                    },
+                    {
+                        label: '日期时间选择',
+                        value: 6,
+                    },
+                    {
+                        label: '日期时间范围选择',
+                        value: 7,
+                    },
+                    {
+                        label: '下拉菜单',
+                        value: 8,
+                    },
+                    {
+                        label: '上传图片',
+                        value: 9,
+                    },
+                    {
+                        label: '上传文件',
+                        value: 10,
+                    },
+                ],
             };
         },
         methods: {
@@ -74,7 +143,12 @@
                     <row>
                         <i-col span="12">
                             <form-item label="信息项类型" prop="type">
-                                <i-input v-model="form.intro"></i-input>
+                                <i-select v-model="form.type">
+                                    <i-option v-for="item in types"
+                                              :disabled="item.value === 0"
+                                              :value="item.value"
+                                              :key="item">{{ item.label }}</i-option>
+                                </i-select>
                             </form-item>
                         </i-col>
                     </row>
@@ -91,7 +165,7 @@
                     <row>
                         <i-col span="12">
                             <form-item label="排序" prop="sort">
-                                <i-input v-model="form.sort"></i-input>
+                                <i-input number v-model="form.sort"></i-input>
                             </form-item>
                         </i-col>
                     </row>
